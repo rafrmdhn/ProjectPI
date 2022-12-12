6300621bb90590e08edd61785803ab7c071e1cc2
Rafi Ramadhan <92287247+rafrmdhn@users.noreply.github.com>
2022-12-12 20:18:29 +0700

    Add files via upload

---
+++ project/Bill.py
@@ -0,0 +1,14 @@
+from Tampilan import Tampilan
+import json
+import Bukti
+
+def Bill(mobil, bayar, date):
+  #menyetak struk tagihan penyewaan
+  bill = open("tagihan.txt", "w")
+  bill.write(Tampilan.date)
+  bill.write('\n')
+  bill.write(Tampilan.appearance[mobil-1])
+  bill.write('\n')
+  bill.write("\nTotal \t\t\t\t\t\tRp. ")
+  bill.write(Tampilan.total)
+  bill.close()  
