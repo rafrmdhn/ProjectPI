a1e6dfe18f6b2dce1d3e252fb8371525f191ab17
Rafi Ramadhan <92287247+rafrmdhn@users.noreply.github.com>
2022-12-15 20:36:06 +0700

    Update RentAmbulance.py

--- project/RentAmbulance.py
+++ project/RentAmbulance.py
@@ -4,17 +4,21 @@ import AmbulanceTersedia
 
 
 def RentAmbulance():
-  # Dapatkan input jenis mobil ambulans yang dipilih 
-  ambulance = int(input("\nMasukkan jenis mobil ambulans (1/2): "))    
-  if (ambulance == 1):
-      ambulance_type = "Mobil Ambulans Umum"
-      Tampilan.Tampilan.ambulance_rate = 500000
-      inputdata(Tampilan.Tampilan.ambulance_rate)
-  elif (ambulance == 2):
-      ambulance_type = "Mobil Ambulans Gabungan"
-      Tampilan.Tampilan.ambulance_rate = 1000000
-      inputdata(Tampilan.Tampilan.ambulance_rate)
-  else:
-      print("Jenis mobil ambulans tidak valid")        
-      print("pilihan invalid masukkan pilihan hanya 1 / 2!")
-      AmbulanceTersedia.AmbulanceTersedia()
+  try:
+    # Dapatkan input jenis mobil ambulans yang dipilih 
+    ambulance = int(input("\nMasukkan jenis mobil ambulans (1/2): "))    
+    if (ambulance == 1):
+        ambulance_type = "Mobil Ambulans Umum"
+        Tampilan.Tampilan.ambulance_rate = 500000
+        inputdata(Tampilan.Tampilan.ambulance_rate)
+    elif (ambulance == 2):
+        ambulance_type = "Mobil Ambulans Gabungan"
+        Tampilan.Tampilan.ambulance_rate = 1000000
+        inputdata(Tampilan.Tampilan.ambulance_rate)
+    else:
+        print("Jenis mobil ambulans tidak valid")        
+        print("pilihan invalid masukkan pilihan hanya 1 / 2!")
+        AmbulanceTersedia.AmbulanceTersedia()
+  except:
+    print("pilihan invalid masukkan pilihan hanya 1 / 2!")
+        AmbulanceTersedia.AmbulanceTersedia()
